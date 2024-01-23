d69a17c736a37b78c2ce715907c5f6ac445327ed
Star <15638276200@163.com>
2024-01-23 20:32:35 +0800

    打包配置调整

--- vue.config.js
+++ vue.config.js
@@ -13,31 +13,37 @@ module.exports = defineConfig({
 
             nodeIntegration: true,
             builderOptions: {
+                //MAC打包需要此配置
                 // electronDownload: {
                 //     // cache: '/Users/cuimingxing/Downloads/electron/Cache',
                 //     cache: '../cache/',
                 //     mirror: 'https://npm.taobao.org/mirrors/electron/',
                 //     customDir: 'v13.6.9-darwin-x64',
                 // },
-                // productName: productName,
+                productName: productName,
                 appId: appId,
 
                 win: {
                     // icon: 'xxx/icon.ico',
+                    icon: './public/icon/task.ico',//桌面快捷方式以及开始程序中的应用图标
+                    artifactName: '${productName}_setup_${version}.${ext}',
                     target: ['nsis']
                   },
                   nsis: {
                     oneClick: false, // 一键安装
+                    menuCategory: menuCategory,
                     // guid: 'xxxx', // 注册表名字，不推荐修改
                     perMachine: true, // 是否开启安装时权限限制（此电脑或当前用户）
                     allowElevation: true, // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
                     allowToChangeInstallationDirectory: true, // 允许修改安装目录
-                    // installerIcon: './build/icons/aaa.ico', // 安装图标
-                    // uninstallerIcon: './build/icons/bbb.ico', // 卸载图标
-                    // installerHeaderIcon: './build/icons/aaa.ico', // 安装时头部图标
-                    createDesktopShortcut: true, // 创建桌面图标
+
+                    installerIcon: './public/icon/system.ico', // 安装图标打包时的exe文件对应的安装图标
+                    uninstallerIcon: './public/icon/vip.ico', // 卸载图标
+                    installerHeaderIcon: './public/icon/icon.ico', // 安装时头部图标
+                    createDesktopShortcut: true, // 创建桌面快捷方式图标
+                    shortcutName: 'I便笺', // 桌面快捷方式图标名称
                     createStartMenuShortcut: true, // 创建开始菜单图标
-                    shortcutName: 'I便笺' // 图标名称
+                    runAfterFinish:true
                   },
 
                 // win: {
